5f46164d868f69951efc21349fba86370e8f430c
nicole-allen679 <nicole.allen679@gmail.com>
2021-05-10 13:03:22 -0400

    implemented sass

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header(){
+    return(
+        <header>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    )
+}
+
+export default Header
---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hero(){
+    return (
+        <div className='hero'>
+            <h2>this is hero</h2>
+        </div>
+    )
+}
+
+export default Hero
